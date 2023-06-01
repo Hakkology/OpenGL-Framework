3681b632140f8155d17b8dec177cbc4816714475
Hakkology <yildizhakan88@gmail.com>
2023-06-02 00:24:33 +0300

    OpenGL Notes added.

---
+++ OpenGLNotes.cpp
@@ -0,0 +1,22 @@
+// GLEW
+// Open GL Extension Wrangler
+// GL3W, glLoadGen, glad, glsdk, glbinding, libepoxy, Glee - some of the libraries
+
+// #include <GL/glew.h>
+
+// glewExperimental = GL_TRUE;
+// glewinit();
+// Should return GLEW_OK, otherwise error.
+// glewGetErrorString(result);
+
+// #include <GL/glfw.h>
+
+// openGL Framework - window creation and control - input
+
+// #include <GL/SDL.h> 
+
+// Simple DirectMedia Layer
+// GLFW + Audio, Threading, Filesystems, etc.
+// Most popular
+// SFML (Simple and Fast Multimedia Library) - Like SDL but even more features
+// Win32 API is the origin
