9c0277e491e88bfd0ecae39114d5d0d50dd93945
Hakkology <yildizhakan88@gmail.com>
2023-06-02 00:23:20 +0300

    Just correcting file locations

---
+++ Program.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace My_Project
+{
+    public class Program
+    {
+        static void Main(string[] args){
+            Console.Writeline("Hello World");
+        }
+        
+    }
+}
