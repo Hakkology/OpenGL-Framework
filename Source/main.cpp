052a737524a29be44faecb5976e6c5c7a61895b1
Hakkology <yildizhakan88@gmail.com>
2023-06-02 22:14:23 +0300

    Main file corrected with documentation

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,32 +1,36 @@
 #include <iostream>
 #include <GLFW/glfw3.h>
 
-int main (int, char**){
-
+int main(void)
+{
     GLFWwindow* window;
 
-    if (!glfwInit()){
+    /* Initialize the library */
+    if (!glfwInit())
         return -1;
-    }
 
-    window = glfwCreateWindow (640, 480, "Window!", NULL, NULL);
-    glfwMakeContextCurrent(window);
-
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
-        std::cout << "Could not load OpenGL" << std::endl;
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
         glfwTerminate();
         return -1;
     }
 
-    glClearColor (0.25f, 0.5f, 0.75f, 1.0f);
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
 
-    while(!glfwWindowShouldClose(window)){
-
-        glfwPollEvents();
-
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
+        /* Swap front and back buffers */
         glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
     }
 
     glfwTerminate();
