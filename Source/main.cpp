7e9ff4aedf1223b70d0179bd25f23eaec3cb1df8
Hakan Yildiz <107430060+Hakkology@users.noreply.github.com>
2023-06-02 11:37:23 +0300

    main code for OpenGL Window

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,9 +1,34 @@
 #include <iostream>
-#include <GLFW/glfw3.h>>
+#include <GLFW/glfw3.h>
 
-int main (){
+int main (int, char**){
 
-    std::cout << "Hello World!" << std::endl;
+    GLFWwindow* window;
 
+    if (!glfwInit()){
+        return -1;
+    }
+
+    window = glfwCreateWindow (640, 480, "Window!", NULL, NULL);
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
+        std::cout << "Could not load OpenGL" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glClearColor (0.25f, 0.5f, 0.75f, 1.0f);
+
+    while(!glfwWindowShouldClose(window)){
+
+        glfwPollEvents();
+
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+    }
+
+    glfwTerminate();
     return 0;
 }
