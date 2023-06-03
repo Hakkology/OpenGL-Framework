1b17d0fe18c3be57c4e078db6ac6db9eafb0de6f
Hakkology <yildizhakan88@gmail.com>
2023-06-03 14:23:26 +0300

    Shaders and Rendering Pipeline Notes-2

--- OpenGLNotes.cpp
+++ OpenGLNotes.cpp
@@ -20,3 +20,47 @@
 // Most popular
 // SFML (Simple and Fast Multimedia Library) - Like SDL but even more features
 // Win32 API is the origin
+// Important for fundamental theory and building up GLFW / GLEW
+
+////////////////////////////
+
+// Rendering Pipelines
+// SEries of stages that take place in order to render an image to the screen.
+// Shaders are vertex shader, fragment shader, geometry shader, tesselation shader and compute shader.
+/*
+1- Vertex Specification
+2- Vertex Shader
+3- Tesselation
+4- Geometry Shader
+5- Vertex Post-Processing
+6- Primitive Assembly
+7- Rasterization
+8- Fragment Shader
+9- Per- Sample Operations
+*/
+
+/* 
+1- Vertex Specification
+A vertex is a point in space, usually defined with x, y and z co-ordinates. 
+Vertex Specification uses these two features:
+- VAO Vertex Array Objects - position, colour, texture, normals etc.
+- VBO defines data itself
+- Attribute pointers define where and how shaders can access vertex data.
+
+Procedure through Code:
+1- Generate a VAO ID
+2- Bind the VAO with that ID
+3- Generate a VBO ID
+4- Bind the VBO with that ID, attaching VBO to VAO
+5- Attach the vertex data to that VBO
+6- Define the Attribute Pointer formatting
+7- Enable the Attribute Pointer
+8- Unbind the VAO and VBO, ready for the next object to be bound
+
+1- Activate Shader Program you want to use
+2- Bind the VAO of object you want to draw
+3- Call glDrawArrays, which initiates the rest of the pipeline
+
+Vertex Shader handles vertices individually.
+Stores data in gl_Position.
+*/
