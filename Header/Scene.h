e1e448245e290cca699b2beb0d95992af52e997b
Hakkology <yildizhakan88@gmail.com>
2023-06-04 23:05:23 +0300

    Keyboard and mouse input added.

--- Header/Scene.h
+++ Header/Scene.h
@@ -45,6 +45,15 @@ private:
     GLint width, height; 
     GLint bufferWidth, bufferHeight; 
 
+    bool keys [1024];
+
+    GLfloat lastX, lastY, xChange, yChange;
+    bool mouseFirstMoved;
+
+    void createCallbacks();
+    static void handleKeys(GLFWwindow* window, int key, int code, int action, int mode);
+    static void handleMouse(GLFWwindow* window, double xPos, double yPos);
+
 #endif
 
 };
--- Source/Scene.cpp
+++ Source/Scene.cpp
@@ -4,12 +4,23 @@ Scene::Scene(){
 
     width = 800;
     height = 600;
+
+    for (size_t i = 0; i < 1024; i++)
+    {
+        keys[i] = 0;
+    }
+    
 }
 
 Scene::Scene(GLint windowWidth, GLint windowHeight){
 
     width = windowWidth;
     height = windowHeight;
+
+    for (size_t i = 0; i < 1024; i++)
+    {
+        keys[i] = 0;
+    }
 }
 
 int Scene::Initialize(){
@@ -47,6 +58,9 @@ int Scene::Initialize(){
     /* Make the window's context current */
     glfwMakeContextCurrent(mainWindow);
 
+    // Handle key + Mouse Input
+    createCallbacks();
+
     // Initialize the GL Extension Wrangler library
     glewExperimental = GL_TRUE;
 
@@ -72,6 +86,56 @@ int Scene::Initialize(){
 
     // Setup Viewport size
     glViewport(0, 0, bufferWidth, bufferHeight);
+
+    glfwSetWindowUserPointer(mainWindow, this);
+}
+
+void Scene::handleKeys(GLFWwindow* window, int key, int code, int action, int mode){
+
+    Scene* theWindow = static_cast<Scene*>(glfwGetWindowUserPointer(window)); 
+
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(window, GL_TRUE);
+    }
+
+    if (key >= 0 && key < 1024)
+    {
+        if (action = GLFW_PRESS)
+        {
+            theWindow ->keys[key] = true;
+        }
+        else if (action == GLFW_RELEASE)
+        {
+            theWindow ->keys[key] = false;
+        }
+    }  
+}
+
+void Scene::handleMouse(GLFWwindow* window, double xPos, double yPos){
+
+    Scene* theWindow = static_cast<Scene*>(glfwGetWindowUserPointer(window)); 
+
+    if (theWindow -> mouseFirstMoved)
+    {
+        theWindow -> lastX = xPos;
+        theWindow -> lastY = yPos;
+        theWindow -> mouseFirstMoved = false;
+    }
+
+    theWindow -> xChange = xPos - theWindow -> lastX;
+    theWindow -> yChange = theWindow -> lastY - yPos;
+
+    theWindow -> lastX = xPos;
+    theWindow -> lastY = yPos;
+
+    printf("x:%.6f, y:%.6f\n", theWindow->xChange, theWindow->yChange);
+}
+
+void Scene::createCallbacks(){
+
+    glfwSetKeyCallback(mainWindow, handleKeys);
+    glfwSetCursorPosCallback(mainWindow, handleMouse);
 }
 
 Scene::~Scene(){
